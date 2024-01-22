8d70e5407da404763a17bd41b33634fccf765159
Skokartong <amanda.olving@gmail.com>
2024-01-22 21:17:59 +0100

    Added test methods for checking that all users get displayed in Services eg.

--- MusicAPITests/UserHelperTests.cs
+++ MusicAPITests/UserHelperTests.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using MusicAPI.Handlers;
+using MusicAPI.Services;
+using MusicAPI.Models;
+using MusicAPI.Data;
 
 namespace MusicAPITests
 {
+    [TestClass]
     internal class UserHelperTests
     {
+
+        [TestMethod]
+        public void GetAllUsers_ReturnsAllUsersInDb()
+        {
+            // Arrange
+            var dbContextOptions = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase(databaseName: "UsersInDatabase")
+                .Options;
+
+            using (var context = new ApplicationContext(dbContextOptions))
+            {
+                context.Users.Add(new User { Id = 1, Name = "User1" });
+                context.Users.Add(new User { Id = 2, Name = "User2" });
+                context.Users.Add(new User { Id = 3, Name = "User3" });
+            }
+
+            using (var context = new ApplicationContext(dbContextOptions))
+            {
+                var userHelper = new UserHelper(context);
+
+                // Act
+                List<User> result = userHelper.GetAllUsers();
+
+                // Assert
+                Assert.IsNotNull(result);
+                Assert.AreEqual(3, result.Count);
+                Assert.IsTrue(result.Any(user => user.Name == "User1"));
+                Assert.IsTrue(result.Any(user => user.Name == "User2"));
+                Assert.IsTrue(result.Any(user => user.Name == "User3"));
+            }
+        }
+
+        [TestMethod]
+        public void GetUser_ReturnsCorrectUser()
+        {
+            // Arrange
+            var dbContextOptions = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase(databaseName: "UserInDatabase")
+                .Options;
+        }
     }
 }
