b293562eb4ee88fed221132c48670335bbfa7567
Skokartong <amanda.olving@gmail.com>
2024-01-26 08:54:21 +0100

    Added MenuOptions folder, Separate user and admin log in functions eg.

--- MusicAPIClient/Handlers/UserHandler.cs
+++ MusicAPIClient/Handlers/UserHandler.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using MusicAPIClient.APIModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MusicAPIClient.Handlers
 {
-    internal class UserHandler
+    // Magda
+    public class UserHandler
     {
+        public static async Task GetUser(HttpClient client, string name)
+        {
+            HttpResponseMessage response = await client.GetAsync("/user/{id}");
+
+            Console.Write("Enter id to view specific user");
+            string input = Console.ReadLine();
+
+            int id;
+
+            if(int.TryParse(input, out id))
+            {
+                try
+                {
+                    var response = await client.GetAsync($"/blog/{id}");
+                }
+
+                catch
+                {
+
+                }
+            }
+        }
     }
 }
