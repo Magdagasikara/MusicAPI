306f263a30e0197b4aa7a0398eed4dfc3df8bbf4
Skokartong <amanda.olving@gmail.com>
2024-01-30 13:35:42 +0100

    Changed menu options and added "Add top 50 songs" and "Add topp 100 artists with top 10 songs"

--- MusicAPIClient/MenuOptions/AdminMenu.cs
+++ MusicAPIClient/MenuOptions/AdminMenu.cs
@@ -63,22 +63,20 @@ namespace MusicAPIClient.MenuOptions
             MenuHelper.HeaderUserAdmin(username);
             Console.WriteLine("1. To view all users");
             Console.WriteLine("2. To add new user");
-            Console.WriteLine("3. To add new song");
-            Console.WriteLine("4. To add new genre");
-            Console.WriteLine("5. To add new artist");
+            Console.WriteLine("3. Add 50 songs from chosen artist");
+            Console.WriteLine("4. Add top 100 artists with their top 10 songs");
+            Console.WriteLine("X. Log out");
 
             // STINA
             // Console.WriteLine("6. Add songs for top 100 (kolla med Stina beskrivning) artists");
             // -- när success: Successfully Added Tracks, Artists and Genres
             // app.MapGet("/spotify/top100/", nån-spotify-handler.StinaMetod utan nån mer input som returnerar IResult)
+            // Top100MostFollowedArtistsTop10Songs
 
             // LUKAS
             // Console.WriteLine("7. Add 50 songs for a chosen artist");
             // -- när success: Successfully Added Tracks, Artist and Genre
             // app.MapGet("/spotify/{artist}/", nån-spotify-handler.LukasMetod som använder ditt artistnamn och returnerar IResult)
-
-
-            Console.WriteLine("X. Log out");
         }
     }
 }
